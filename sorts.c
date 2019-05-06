a9c607752eb940c473cb4b7706042490579bf0a7
Justin Wise <jrwise200@gmail.com>
2019-05-05 23:40:32 -0500

    Added heapsort

--- sorts.c
+++ sorts.c
@@ -2,24 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
+#include <stdbool.h>
 
-void sort(int n, int* arr, void (*sort)(int, int*));
-void selectionSort(int n, int* arr);
-void insertionSort(int n, int* arr);
-void mergeSort(int n, int* arr);
-void mergeHelper(int n, int* arr, int start, int end);
+void sort(int n, int* arr, void (*sort)(int, int*, bool));
+void selectionSort(int n, int* arr, bool show);
+void insertionSort(int n, int* arr, bool show);
+void mergeSort(int n, int* arr, bool show);
+void mergeHelper(int n, int* arr, int start, int end, bool show);
 void merge(int n, int*arr, int start1, int start2, int end);
-void quickSort(int n, int* arr);
-void quickSortHelper(int n, int* arr, int start, int end);
-void heapSort(int n, int* arr);
+void quickSort(int n, int* arr, bool show);
+void quickSortHelper(int n, int* arr, int start, int end, bool show);
+void heapSort(int n, int* arr, bool show);
+int left(int i) {return 2 * i + 1;}
+int right(int i) {return 2 * i + 2;}
+int parent(int i) {return (i-1) / 2;}
 void swap(int i, int j, int* arr);
 void print(int n, int* arr);
 void printPivot(int n, int* arr, int pivot);
 int* copy(int n, int* arr);
+void randomSorts();
+void verify(void (*sort)(int, int*, bool));
 
 
 int main() {
 	
+	randomSorts();
+	
+	return 0;
+}
+
+void verify(void (*sort)(int, int*, bool)) {
+	
+}
+
+void randomSorts() {
 	srand(time(0));
 	
 	int n;
@@ -36,13 +52,12 @@ int main() {
 	sort(n, arr, insertionSort);
 	sort(n, arr, mergeSort);
 	sort(n, arr, quickSort);
+	sort(n, arr, heapSort);
 	
 	free(arr);
-	
-	return 0;
 }
 
-void sort(int n, int* arr, void (*sort)(int, int*)) {
+void sort(int n, int* arr, void (*sort)(int, int*, bool)) {
 	arr = copy(n, arr);
 	sort(n, arr);
 	free(arr);
@@ -57,11 +72,11 @@ void print(int n, int* arr) {
 	printf("%02d]\n", arr[n-1]);
 }
 
-void printPivot(int n, int* arr, int pivot) {
+void printColor(int n, int* arr, int start, int end) {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	printf("[");
 	for (int i = 0; i < n - 1; i++) {
-		if (i != pivot) 
+		if (i < start || i >= end) 
 			printf("%02d, ", arr[i]);
 		else {
 			SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
@@ -69,7 +84,14 @@ void printPivot(int n, int* arr, int pivot) {
 			SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 		}
 	}
-	printf("%02d]\n", arr[n-1]);
+	if (n-1 < start || n-1 >= end) 
+		printf("%02d]\n", arr[n-1]);
+	else {
+		SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+		printf("%02d", arr[n-1]);
+		SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		printf("]\n");
+	}
 }
 
 int* copy(int n, int*arr) {
@@ -86,7 +108,7 @@ void swap(int i, int j, int* arr) {
 	arr[i] = temp;
 }
 
-void selectionSort(int n, int* arr) {
+void selectionSort(int n, int* arr, bool show) {
 	printf("\nSelection Sort:\n");
 	print(n, arr);
 	for (int i = 0; i < n - 1; i++) {
@@ -101,7 +123,7 @@ void selectionSort(int n, int* arr) {
 	}
 }
 
-void insertionSort(int n, int* arr) {
+void insertionSort(int n, int* arr, bool show) {
 	printf("\nInsertion Sort:\n");
 	print(n, arr);
 	for (int i = 1; i < n; i++) {
@@ -114,13 +136,13 @@ void insertionSort(int n, int* arr) {
 	}
 }
 
-void mergeSort(int n, int* arr) {
+void mergeSort(int n, int* arr, bool show) {
 	printf("\nMerge Sort:\n");
 	print(n, arr);
 	mergeHelper(n, arr, 0, n);
 }
 
-void mergeHelper(int n, int* arr, int start, int end) {
+void mergeHelper(int n, int* arr, int start, int end, bool show) {
 	if (end - start > 1) {
 		int mid = (start + end) / 2;
 		mergeHelper(n, arr, start, mid);
@@ -165,14 +187,14 @@ void merge(int n, int* arr, int start1, int start2, int end) {
 	free(temp);
 }
 
-void quickSort(int n, int* arr) {
+void quickSort(int n, int* arr, bool show) {
 	printf("\nQuick Sort:\n");
 	print(n, arr);
 	quickSortHelper(n, arr, 0, n);
 	print(n, arr);
 }
 
-void quickSortHelper(int n, int* arr, int start, int end) {
+void quickSortHelper(int n, int* arr, int start, int end, bool show) {
 	if (end - start > 1) {
 		int i = start;
 		for (int j = start; j < end - 1; j++) {
@@ -182,8 +204,39 @@ void quickSortHelper(int n, int* arr, int start, int end) {
 			}
 		}
 		swap(i, end-1, arr);
-		printPivot(n, arr, i);
+		printColor(n, arr, i, i+1);
 		quickSortHelper(n, arr, start, i);
 		quickSortHelper(n, arr, i+1, end);
 	}
+}
+
+void heapSort(int n, int* arr, bool show) {
+	printf("\nHeap Sort:\n");
+	print(n, arr);
+	//Build heap
+	for (int i = 1; i < n; i++) {
+		 int j = i;
+		 while(j > 0 && arr[j] > arr[parent(j)]) {
+			 swap(j, parent(j), arr);
+			 j = parent(j);
+		 }
+	}
+	print(n, arr);
+	//Sort array
+	for (int i = n-1; i > 0; i--) {
+		swap(0, i, arr);
+		int j = 0;
+		while(left(j) < i && arr[j] < arr[left(j)] || right(j) < i && arr[j] < arr[right(j)]) {
+			if (right(j) < i) {
+				int c = arr[left(j)] > arr[right(j)] ? left(j) : right(j);
+				swap(j, c, arr);
+				j = c;
+			} else {
+				swap(j, left(j), arr);
+				j = left(j);
+			}
+		}
+		printColor(n, arr, i, n);
+	}
+	print(n, arr);
 }
